21420dc7f894a0e7243ef1ebf5533635a2de4fe1
Ayush Jain <77970015+not4YU5H@users.noreply.github.com>
2022-06-02 03:05:23 +0530

    Initial commit + Implementation almost complete

---
+++ app/src/main/java/com/example/sampleimgencrypapp/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.sampleimgencrypapp
+
+import android.Manifest
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.DexterBuilder
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.example.sampleimgencrypapp.MainActivity.*
+import com.example.sampleimgencrypapp.Utils.MyEncrypter
+import java.io.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var myDir:File
+
+    companion object {
+        private val FILE_NAME_ENC = "test_enc"
+        private val FILE_NAME_DEC = "test_dec.jpg"
+
+        private val key="poOkWuKk9Be1Hq01" //16 char = 128 bit random unique key
+        private val specString = "H3yT5Cx1FDw1Uci0" //16 char = 128 bit
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btn_enc = findViewById(R.id.btn_enc) as Button
+        var btn_dec = findViewById(R.id.btn_dec) as Button
+
+        Dexter.withActivity(this)
+            .withPermissions(*arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ))
+            .withListener(object:MultiplePermissionsListener{
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    btn_enc.isEnabled = true
+                    btn_dec.isEnabled = true
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    Toast.makeText(this@MainActivity, "You should accept permission", Toast.LENGTH_SHORT).show()
+                }
+            })
+            .check()
+
+        val root = Environment.getExternalStorageDirectory().toString()
+        myDir = File("$root/save_images")
+        if(!myDir.exists())
+            myDir.mkdirs()
+
+        btn_enc.setOnClickListener {
+            //Converting drawable to bitmap
+            val drawable = ContextCompat.getDrawable(this@MainActivity,R.drawable.test)
+            val bitmapDrawable = drawable as BitmapDrawable
+            val bitmap = bitmapDrawable.bitmap
+            val stream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream)
+            val input= ByteArrayInputStream(stream.toByteArray())
+            val outputFileEnc = File(myDir, FILE_NAME_ENC) // Create empty file enc
+
+            try {
+                MyEncrypter.encryptToFile(key, specString, input, FileOutputStream(outputFileEnc))
+                Toast.makeText(this@MainActivity, "Encrypted", Toast.LENGTH_SHORT).show()
+            }
+            catch (e:Exception)
+            {
+                e.printStackTrace()
+            }        }
+
+        btn_dec.setOnClickListener {
+            val outputFileDec = File(myDir, FILE_NAME_DEC) //Creates empty file dec
+            val encFile = File(myDir, FILE_NAME_ENC)
+            TODO()
+//            try {
+//                MyEncrypter.decryptToFile(key, specString, FileInputStream(encFile), FileOutputStream(outputFileDec))
+//
+//
+//            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sampleimgencrypapp/Utils/MyEncrypter.kt
@@ -0,0 +1,61 @@
+package com.example.sampleimgencrypapp.Utils
+
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+import java.security.InvalidAlgorithmParameterException
+import java.security.InvalidKeyException
+import java.security.NoSuchAlgorithmException
+import javax.crypto.Cipher
+import javax.crypto.CipherOutputStream
+import javax.crypto.NoSuchPaddingException
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+import kotlin.jvm.Throws
+
+object MyEncrypter {
+    private val DEFAULT_READ_WRITE_BLOCK_BUFFER_SIZE = 1024
+    private val ALGO_IMAGE_ENCRYPTOR = "AES/CBC/PKCS5Padding"
+    private val ALGO_SECRET_KEY = "AES"
+
+    @Throws(NoSuchAlgorithmException::class, NoSuchPaddingException::class, InvalidKeyException::class, IOException::class, InvalidAlgorithmParameterException::class)
+    fun encryptToFile (keyStr:String, spec:String, input:InputStream, output:OutputStream)
+    {
+        var output = output
+        try {
+            val iv = IvParameterSpec(spec.toByteArray(charset("UTF-8")))
+            val keySpec = SecretKeySpec(keyStr.toByteArray(charset("UTF-8")), ALGO_SECRET_KEY)
+            val c = Cipher.getInstance(ALGO_IMAGE_ENCRYPTOR)
+            c.init(Cipher.ENCRYPT_MODE,keySpec,iv)
+            output = CipherOutputStream(output, c)
+            val buffer = ByteArray(DEFAULT_READ_WRITE_BLOCK_BUFFER_SIZE)
+            var bytesRead:Int = 0
+            while(input.read(buffer).also { bytesRead = it }>0)
+                output.write(buffer, 0, bytesRead)
+        }
+        finally {
+            output.close()
+        }
+    }
+
+    fun decryptToFile (keyStr:String, spec:String, input:InputStream, output:OutputStream)
+    {
+        var output = output
+        try {
+            val iv = IvParameterSpec(spec.toByteArray(charset("UTF-8")))
+            val keySpec = SecretKeySpec(keyStr.toByteArray(charset("UTF-8")), ALGO_SECRET_KEY)
+            val c = Cipher.getInstance(ALGO_IMAGE_ENCRYPTOR)
+            c.init(Cipher.DECRYPT_MODE,keySpec,iv)
+            output = CipherOutputStream(output, c)
+            val buffer = ByteArray(DEFAULT_READ_WRITE_BLOCK_BUFFER_SIZE)
+            var bytesRead:Int = 0
+            while(input.read(buffer).also { bytesRead = it }>0)
+                output.write(buffer, 0, bytesRead)
+        }
+        finally {
+            output.close()
+        }
+    }
+}
+
+}
